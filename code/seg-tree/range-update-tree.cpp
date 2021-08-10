eaddb5d35c888da42b4187b2eb8855f34d792feb
akcube <a.kishorekumar@students.iiit.ac.in>
2021-08-11 02:17:32 +0300

    Improved file structure, templatized segtrees & tries, cleaned up LCA implementation

--- code/seg-tree/range-update-tree.cpp
+++ code/seg-tree/range-update-tree.cpp
@@ -1,22 +1,23 @@
+template <typename T>
 class RangeUpdateTree{
 private: 
     int n;
-    vector<int> tree;
-    int identity;
+    vector<T> tree;
+    T identity;
 
-    int merge(int a, int b){
+    T merge(T a, T b){
         return a + b;
     }
 
-    int __query(int q){
-        int accum = identity;
+    T __query(int q){
+        T accum = identity;
         for(int i=n+q; i > 0; i /= 2){
             accum = merge(tree[i], accum);
         }
         return accum;
     }
 
-    void __update(int node, int seg_l, int seg_r, int q_l, int q_r, int val){
+    void __update(int node, int seg_l, int seg_r, int q_l, int q_r, T val){
         if(seg_l > q_r || seg_r < q_l) return;
         if(seg_l >= q_l && seg_r <= q_r) {
             tree[node] += val;
@@ -27,7 +28,7 @@ private:
         __update(2*node+1, mid+1, seg_r, q_l, q_r, val);
     }
 
-    void build(vector<int> &arr){
+    void build(vector<T> &arr){
         n = 1 << (32 - __builtin_clz ((int)arr.size() - 1));
         tree.resize(2*n);
         for(int i=0; i<arr.size(); i++)
@@ -35,16 +36,16 @@ private:
     }
 
 public:
-    RangeUpdateTree(vector<int> &arr, int id){
+    RangeUpdateTree(vector<T> &arr, T id){
         identity = id;
         build(arr);
     }
 
-    int query(int pos){
+    T query(int pos){
         return __query(pos);
     }
 
-    void update(int l, int r, int value){
+    void update(int l, int r, T value){
         __update(1, 0, n-1, l, r, value);
     }
 };
--- code/seg-tree/segtree.cpp
+++ code/seg-tree/segtree.cpp
@@ -1,21 +1,22 @@
+template <typename T>
 class Segtree{
 private: 
     int n;
-    vector<int> tree;
-    int identity;
+    vector<T> tree;
+    T identity;
 
-    int merge(int a, int b){
+    T merge(T a, T b){
         return a + b;
     }
 
-    int __query(int node, int seg_l, int seg_r, int q_l, int q_r){
+    T __query(int node, int seg_l, int seg_r, int q_l, int q_r){
         if(seg_l >= q_l && seg_r <= q_r) return tree[node];
         if(seg_l > q_r || seg_r < q_l) return identity;
         int mid = seg_l + (seg_r-seg_l)/2;
         return merge(__query(2*node, seg_l, mid, q_l, q_r), __query(2*node+1, mid+1, seg_r, q_l, q_r));
     }
 
-    void build(vector<int> &arr){
+    void build(vector<T> &arr){
         int sz = (int) arr.size();
         n = 1 << (32 - __builtin_clz (sz - 1));
         tree.resize(2*n);
@@ -27,16 +28,16 @@ private:
     }
 
 public:
-    Segtree(vector<int> &arr, int id){
+    Segtree(vector<T> &arr, T id){
         identity = id;
         build(arr);
     }
 
-    int query(int l, int r){
+    T query(int l, int r){
         return __query(1, 0, n-1, l, r);
     }
 
-    void update(int node, int value){
+    void update(int node, T value){
         tree[n+node] = value;
         for(int i=(n+node)/2; i>=1; i/=2)
             tree[i] = merge(tree[2*i], tree[2*i+1]);
