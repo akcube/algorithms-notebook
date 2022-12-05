ca831955438d3488005e1955b11523c00718cc48
akcube <a.kishorekumar@students.iiit.ac.in>
2022-12-05 13:32:53 +0530

    KACTL style single array dsu

--- code/graph/dsu.cpp
+++ code/graph/dsu.cpp
@@ -1,23 +1,13 @@
 struct DSU{
-    vi dsu, szx;
-
-    DSU() = default;
-    DSU(int n) : dsu(n), szx(n, 1) { 
-        for(int i=0; i<n; i++) dsu[i] = i;
-    }
-    
-    int parent(int i){
-        if(dsu[i]==i) return i;
-        else return dsu[i] = parent(dsu[i]);
-    }
-
-    int size(int i) { return szx[parent(i)]; }
+    vi dsu;
+    int num_comps;
+    DSU(int n) : dsu(n, -1), num_comps(n) {}
+    int parent(int i){ return dsu[i] < 0 ? i : dsu[i] = parent(dsu[i]); }
+    int size(int i) { return -dsu[parent(i)]; }
     int operator[](int i){ return parent(i); }
-
     void unify(int a, int b){
-        a = parent(a);
-        b = parent(b);
-        if(szx[a] < szx[b]) swap(a, b);
-        if(a!=b) dsu[b] = a, szx[a] += szx[b];
+        a = parent(a), b = parent(b);
+        if(dsu[a] > dsu[b]) swap(a, b);
+        if(a!=b) dsu[a] += dsu[b], dsu[b] = a, num_comps--;
     }
 };
