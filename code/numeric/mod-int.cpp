4a5eb2c7735c594b200160587720b6bbc65a298f
akcube <a.kishorekumar@students.iiit.ac.in>
2023-03-08 16:57:13 +0530

    minor fixes

--- code/numeric/mod-int.cpp
+++ code/numeric/mod-int.cpp
@@ -1,6 +1,6 @@
 template<const int mod>
 struct mint {
-    mint(int x = 0) : val((x % mod + mod) % mod) {}
+    constexpr mint(int x = 0) : val((x % mod + mod) % mod) {}
     mint& operator+=(const mint &b) { val += b.val; val -= mod * (val >= mod); return *this; }
     mint& operator-=(const mint &b) { val -= b.val; val += mod * (val < 0); return *this; }
     mint& operator*=(const mint &b) { val = 1ll * val * b.val % mod; return *this; }
@@ -11,13 +11,18 @@ struct mint {
             t = a/b, a -= t * b, x -= t * y;
         return mint(x);
     }
+    mint pow(int b) const {
+        mint a = *this, res(1);
+        for(; b; a *= a, b /= 2)  if(b&1) res *= a;
+        return res;
+    }
     friend mint operator+(const mint &a, const mint &b) { return mint(a) += b; }
     friend mint operator-(const mint &a, const mint &b) { return mint(a) -= b; }
     friend mint operator*(const mint &a, const mint &b) { return mint(a) *= b; }
     friend mint operator/(const mint &a, const mint &b) { return mint(a) /= b; }
     friend bool operator==(const mint &a, const mint &b) { return a.val == b.val; }
     friend bool operator!=(const mint &a, const mint &b) { return a.val != b.val; }
-    friend std::ostream& operator<<(std::ostream &os, const mint &a) { return os << a.val; }
+    friend ostream& operator<<(ostream &os, const mint &a) { return os << a.val; }
     int val;
 };
 using Mint = mint<MOD>;
