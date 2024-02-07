951d332aac60fc05fd9f2b4d7df1ec787a787870
akcube <a.kishorekumar@students.iiit.ac.in>
2024-02-08 04:22:30 +0530

    Code cleanup

--- code/trie/tries-arr.cpp
+++ code/trie/tries-arr.cpp
@@ -1,31 +1,21 @@
 const int NX = int(1e6) + int(5e5);
 int arr[NX][26];
-int root;
 int lastocc;
-
 void Trie(){
-    root = 0;
     lastocc = 0;
-    memset(arr, 0, sizeof(int)*NX*26);
+    memset(arr, 0, sizeof(arr));
 }
-
 void insert(const string &x){
-    int curptr = root;
-
+    int curptr = 0;
     for(auto ch:x){
-        if(arr[curptr][ch-'a']==0)
-            arr[curptr][ch-'a'] = ++lastocc;
+        if(arr[curptr][ch-'a']==0) arr[curptr][ch-'a'] = ++lastocc;
         curptr = arr[curptr][ch-'a'];
     }
 }
-
 int search(const string &x){
-    int curptr = root;
-    for(auto ch:x){
-        if(arr[curptr][ch-'a']==0)
-            return 0;
-        else
-            curptr = arr[curptr][ch-'a'];
-    }
+    int curptr = 0;
+    for(auto ch:x)
+        if(arr[curptr][ch-'a']==0) return 0;
+        else curptr = arr[curptr][ch-'a'];
     return 1;
 }
